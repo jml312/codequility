17d1788ce2e722d30a224677c78c830dd4f23ab5
Josh Levy <>
2023-01-04 23:40:22 -0500

    simple auth setup

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,14 @@
-import SvelteKitAuth from "@auth/sveltekit";
+import { SvelteKitAuth } from "@auth/sveltekit";
 import GitHub from "@auth/core/providers/github";
 import { GITHUB_ID, GITHUB_SECRET } from "$env/static/private";
+import type { Provider } from "@auth/core/providers";
+import type { Profile } from "@auth/core/types";
 
 export const handle = SvelteKitAuth({
-	providers: [GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET })]
+	providers: [
+		GitHub({
+			clientId: GITHUB_ID,
+			clientSecret: GITHUB_SECRET
+		})
+	] as Provider<Profile>[]
 });
